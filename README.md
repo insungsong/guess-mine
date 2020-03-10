f710f3931d6c8a4dca7e4fcad7e8dcefc5d9c9dc
insungsong <song22861@naver.com>
2020-03-10 17:37:31 +0900

    환경설정

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # guess-mine
+
 RealTime Drawing Game built with SocetIO, gulp and Node
+
+## TO DO:
+
+- [x] Server
+- [] Pug
+- [] Static
+- [] SocketIO
+-
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { join } from "path";
+import socketIo from "socket.io";
+
+const PORT = 5000;
+const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const handleListening = () => {
+  console.log(`Server running: http://localhost:${PORT}`);
+};
+
+const server = app.listen(PORT, handleListening);
+
+const io = socketIo(server);
